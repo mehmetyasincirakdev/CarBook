baa6dc9ee375f328d04ed2dba732f21938ecc32a
mehmetyasincirakdev <myasincirak@gmail.com>
2024-05-09 00:59:12 +0300

    Add project files.

---
+++ Core/CarBook.Domain/Entities/CarFeatures.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class CarFeatures
+    {
+        public int CarFeatureId { get; set; }
+        public string CarId { get; set;}
+        public Car Car { get; set; }
+        public int FeatureId { get; set; }
+        public Feature Feature { get; set; }
+        public bool Available { get; set; }
+    }
+}
