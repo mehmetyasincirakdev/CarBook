5f25f01e98223fb72eecbac8480ffc1c387f39ab
mehmetyasincirakdev <myasincirak@gmail.com>
2024-05-15 18:46:26 +0300

    fixed

--- Infrastructure/CarBook.Persistence/Context/CarBookContext.cs
+++ Infrastructure/CarBook.Persistence/Context/CarBookContext.cs
@@ -12,23 +12,24 @@ namespace CarBook.Persistence.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\mssqllocaldb;initial Catalog=CarBook;integrated security=true;");
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;initial Catalog=CarBook;integrated security=true;");
         }
         public DbSet<About> Abouts { get; set; }
-        public DbSet<Banner> Banners { get; set; }
-        public DbSet<Car> Cars { get; set; }
+        public DbSet<Banner> Banners{ get; set; }
         public DbSet<Brand> Brands { get; set; }
-        public DbSet<Pricing> Pricings { get; set; }
+        public DbSet<Car> Cars { get; set; }
         public DbSet<CarDescription> CarDescriptions { get; set; }
-        public DbSet<Category> Categories { get; set; }
+        public DbSet<CarFeatures> CarFeatures { get; set; } 
         public DbSet<CarPricing> CarPricings { get; set; }
+        public DbSet<Category> Categories { get; set; } 
         public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
         public DbSet<FooterAddress> FooterAddresses { get; set; }
         public DbSet<Location> Locations { get; set; }
-        public DbSet<Feature> Features { get; set; }
+        public DbSet<Pricing> Pricings { get; set; }
         public DbSet<Service> Services { get; set; }
         public DbSet<SocialMedia> SocialMedias { get; set; }
         public DbSet<Testimonial> Testimonials { get; set; }
-        public DbSet<CarFeatures> CarFeatures { get; set; }
+        
     }
 }
